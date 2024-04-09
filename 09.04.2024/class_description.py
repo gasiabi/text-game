6b2e2c2e3b3fc5187f22a001494bafec8ca5d36f
Basia <151082607+gasiabi@users.noreply.github.com>
2024-04-09 12:10:36 +0200

    Add files via upload

---
+++ 09.04.2024/class_description.py
@@ -0,0 +1,31 @@
+knight_description = "The knight is a stalwart defender and valiant leader on the battlefield, " \
+                     "\nclad in heavy armor and wielding a mighty weapon. " \
+                     "\nThey excel in strategic warfare, inspiring allies and standing " \
+                     "\nfirm against adversaries with unwavering courage and resilience." \
+                     "\n" \
+                     "\nThe best with: human"
+
+archer_description = "The archer is a skilled marksman, wielding a bow " \
+                     "\nwith precision and finesse on the battlefield. " \
+                     "\nThey excel in ranged combat, delivering deadly accuracy from a distance. " \
+                     "\nAgile and resourceful, they provide strategic support to allies " \
+                     "\nwhile keeping adversaries at bay with swift and calculated strikes." \
+                     "\n" \
+                     "\nThe best with: anitur"
+
+mage_description = "The mage harnesses the arcane energies of the universe, " \
+                   "\nweaving spells of immense power and mystery. " \
+                   "\nMasters of the magical arts, they command elemental forces to manipulate the battlefield," \
+                   "\nunleashing devastating attacks or providing invaluable support to allies. " \
+                   "\nWith their intellect and mastery of magic, they are a force to be reckoned with." \
+                   "\n" \
+                   "\nThe best with: elf, venif"
+
+fighter_description = "The fighter is a relentless combatant, honing their skills " \
+                      "\nin hand-to-hand combat and mastering various weapons. " \
+                      "\nFearless and ferocious, they charge into battle " \
+                      "\nwith unmatched strength and determination. " \
+                      "\nVersatile and adaptable, they can adapt their fighting style to any situation, " \
+                      "\nmaking them a formidable adversary on the battlefield." \
+                      "\n" \
+                      "\nThe best with: human, anitur"
---
+++ 09.04.2024/race_description.py
@@ -0,0 +1,58 @@
+human_description = "Humans may be portrayed as fragile creatures, " \
+                    "but their adaptability and the ambition they can possess are incomparable." \
+                    "\nIf you decide to choose human as your character's race, you must know that " \
+                    "despite your amazing strategic and physical skills, \nYour life depends only on your ability to learn." \
+                    "\nYou will have no magical abilities or unnatural senses. " \
+                    "\n" \
+                    "\n     4/5 in Strategic Points" \
+                    "\n     4/5 in Mendacity Points" \
+                    "\n     1/5 in Magic Points" \
+                    "\n     2/5 in Agility Points" \
+                    "\n     1/5 in Senses Points" \
+                    "\n     5/5 in Physical Points"
+
+elf_description = "Elves are known for their grace and loyalty towards each other.\n" \
+                  "For centuries, they have endeavored to live in harmony with the essence of their beings, " \
+                  "\nguided by ancestral morals passed down from generation to generation." \
+                  "\nIf you choose elf as the race of your character," \
+                  "\nyou embrace a life deeply connected to nature and the arcane arts." \
+                  "\nYou possess incredible intelligence and unmatched magical skills, honed over centuries of existence." \
+                  "\nHowever your physical resilience and mendacity may pale in comparison to other races." \
+                  "\n" \
+                  "\n     4/5 in Strategic Points" \
+                  "\n     1/5 in Mendacity Points" \
+                  "\n     5/5 in Magic Points" \
+                  "\n     2/5 in Agility Points" \
+                  "\n     3/5 in Senses Points" \
+                  "\n     1/5 in Physical Points"
+
+
+anitur_description = "Over the centuries more and more Aniturs could be spotted in the wild magical forests of Eldoria." \
+                     "\nPossessing human-like traits, they astound with their acute senses, " \
+                     "\nhoned to perfection through their deep connection to the primal essence of the wilderness." \
+                     "\nDespite their human characteristics, they have incredibly keen senses, " \
+                     "\nand their agility is unmatched by any other creature." \
+                     "\nThey are not the friendliest, yet they sometimes can be spotted chatting with elves." \
+                     "\n" \
+                     "\n     1/5 in Strategic Points" \
+                     "\n     1/5 in Mendacity Points" \
+                     "\n     1/5 in Magic Points" \
+                     "\n     3/5 in Agility Points" \
+                     "\n     5/5 in Senses Points" \
+                     "\n     5/5 in Physical Points"
+
+
+venif_description = "In the enigmatic lands of Eldoria, dwell the Venifians, a unique breed of humans endowed with magical prowess."\
+                    "\nAmidst their robust physical capabilities and strategic finesse, lies a latent potential for mystical arts."\
+                    "\nWhile their magical abilities are nascent, they possess an innate affinity for manipulating the arcane forces."\
+                    "\nThough their agility and sensory perception may be lacking, their determination and adaptability shine through."\
+                    "\nBound by a shared heritage, they navigate the complexities of their world with resilience and ambition." \
+                    "\n"\
+                    "\n     2/5 in Strategic Points" \
+                    "\n     3/5 in Mendacity Points" \
+                    "\n     5/5 in Magic Points" \
+                    "\n     1/5 in Agility Points" \
+                    "\n     1/5 in Senses Points" \
+                    "\n     4/5 in Physical Points"
+
+races = [human_description, elf_description, anitur_description, venif_description]
